ce4682236dd624217051b259f24e0ced0a2cf29a
Joseph Evangelista <oootep123@gmail.com>
2018-08-11 19:54:56 -0400

    added files to be used

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var exServer = express();
+// set initial port
+var PORT = process.env.PORT || 3000;
+//import modules
+require( './routes/htmlRoutes.js' )( exServer );
+require( './routes/apiRoutes.js' )( exServer );
+//---------- Server start listen ----------
+exServer.listen( PORT, function() {
+    console.log( 'App listening on PORT ' + PORT );
+});
