a277c095b69e5d6ee5a04a813ed2524512afddca
Joseph Evangelista <oootep123@gmail.com>
2018-08-13 20:12:20 -0400

    apiroutes still not complete

---
+++ routing/apiRoutes.js
@@ -0,0 +1,16 @@
+var friendsArray = require("../app/data/friends");
+
+module.exports = function(app) {
+
+  app.get("/api/friends", function(req, res) {
+    res.json(friendsArray);
+  });
+
+  app.post("/api/friends", function(req, res) {
+  
+   
+  });
+
+
+ 
+};
--- server.js
+++ server.js
@@ -3,8 +3,8 @@ var exServer = express();
 // set initial port
 var PORT = process.env.PORT || 3000;
 //import modules
-require( './routes/htmlRoutes.js' )( exServer );
-require( './routes/apiRoutes.js' )( exServer );
+require( './routing/htmlRoutes.js' )( exServer );
+require( './routing/apiRoutes.js' )( exServer );
 //---------- Server start listen ----------
 exServer.listen( PORT, function() {
     console.log( 'App listening on PORT ' + PORT );
