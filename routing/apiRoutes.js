2d0570e552128a4bb037c45e63a6986f517ab820
Joseph Evangelista <oootep123@gmail.com>
2018-08-18 20:56:43 -0400

    added post function in api routing

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -6,10 +6,27 @@ module.exports = function(app) {
     res.json(friendsArray);
   });
 
-  app.post("/api/friends", function(req, res) {
-  
-   
-  });
+  app.post('/api/friends', function(req, res) {
+		var userInput = req.body;
+		var userResponses = userInput.scores;
+		var matchName = '';
+		var matchImage = '';
+		var totalDifference = 10000;
+		for (var i = 0; i < friendsArray .length; i++) {
+
+			var diff = 0;
+			for (var j = 0; j < userResponses.length; j++) {
+				diff += Math.abs(friendsArray [i].scores[j] - userResponses[j]);
+			}
+			if (diff < totalDifference) {
+				totalDifference = diff;
+				matchName = friendsArray [i].name;
+				matchImage = friendsArray [i].photo;
+			}
+		}
+		friendsArray .push(userInput);
+		res.json({status: 'OK', matchName: matchName, matchImage: matchImage});
+	});
 
 
  
